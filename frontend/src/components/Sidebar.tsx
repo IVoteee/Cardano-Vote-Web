06991243fd8f5961d6353f7b8185cbb2cbe2272d
Nguyen Khanh <nguyenkhanh17112003@gmail.com>
2023-03-25 11:24:11 +0700

    Create frontend

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { BsArrowLeftShort, BsSearch, BsChevronDown, BsFillImageFill, BsReverseLayoutTextSidebarReverse, BsPerson } from "react-icons/bs"
+import { AiFillEnvironment, AiOutlineBarChart, AiOutlineFileText, AiOutlineLogout, AiOutlineMail, AiOutlineSetting } from "react-icons/ai"
+import { RiDashboardFill } from "react-icons/ri"
+
+const Sidebar = () => {
+    const [open, setOpen] = useState(true);
+    const [submenuOpen, setSubmenuOpen] = useState(true);
+    const Menus = [
+        { title: "Dashboard" },
+        { title: "Pages" , icon: <AiOutlineFileText/> },
+        { title: "Media", spacing: true, icon: <BsFillImageFill/> },
+        {
+            title: "Projects", icon: <BsReverseLayoutTextSidebarReverse/>,
+            submenu: true,
+            submenuItems: [
+                { title: "Submenu 1" },
+                { title: "Submenu 2" },
+                { title: "Submenu 3" }
+            ],
+        },
+        { title: "Analytics", icon: <AiOutlineBarChart/> },
+        { title: "Inbox", icon: <AiOutlineMail/> },
+        { title: "Profile", spacing: true, icon: <BsPerson/> },
+        { title: "Setting", icon: <AiOutlineSetting/> },
+        { title: "Logout", icon: <AiOutlineLogout/> },
+    ];
+  return (
+    <div className='flex'>
+            <div className={`bg-blue-500 h-screen p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300 relative`}>
+                <BsArrowLeftShort className={`bg-white bg-blue-500 text-3xl rounded-full absolute -right-3 top-9 border border-dark-blue cursor-pointer ${!open && "rotate-180"}`}
+                onClick={() => setOpen(!open)}/>
+                <div className='inline-flex'>
+                    <AiFillEnvironment className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2 duration-500 ${open && "rotate-[360deg]"}`}/>
+                    <h1 className={`text-white origin-left font-bold text-2xl duration-300 ${!open && "scale-0"}`}>iVote</h1>
+                </div>
+                <div className={`flex items-center rounded-md bg-light-white bg-gray-200 mt-6 ${!open ? "px-2.5" : "px-4"} py-2`}>
+                    <BsSearch className={`text-blue-500 text-lg block float-left cursor-pointer ${open && "mr-2"}`}/>
+                    <input type={"search"} placeholder="Tìm kiếm" className={`text-base bg-transparent w-full text-dark focus:outline-none ${!open && "hidden"}`} />
+                </div>
+                <ul className='pt-2'>
+                {Menus.map((menu, index) => (
+                    <>
+                        <li key={index} className={`text-white text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md mt-2 ${menu.spacing ? "mt-9" : "mt-2"}`}>
+                            <span className='text-2xl block float-left'>
+                                {menu.icon ? menu.icon : <RiDashboardFill/>}
+                            </span>
+                            <span className={`text-base font-medium flex-1 duration-200 ${!open && "hidden"}`}>{menu.title}</span>
+                            {menu.submenu && open && (
+                                <BsChevronDown className={`${submenuOpen && "rotate-180"}`} onClick={() => setSubmenuOpen(!submenuOpen)}/>
+                            )}
+                        </li>
+
+                        {menu.submenu && submenuOpen && open && (
+                            <ul>
+                                {menu.submenuItems.map((submenuItem, index) => (
+                                    <li key={index} className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md">
+                                        {submenuItem.title}
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </>
+                ))}
+            </ul>
+            </div>
+        <div className='p-7'>
+            <div className='text-2xl font-semibold'>Home page</div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Footer from '@/components/Footer'
+
+const login = () => {
+  return (
+    <>
+      <div className='body-bg m-auto py-5 h-full'>
+        <header className='max-w-lg mx-auto py-10'>
+          <h1 className='text-4xl font-bold text-white text-center'>iVote</h1>
+        </header>
+        <div className='bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl align-middle my-auto'>
+            <section className='text-center'>
+              <h3 className='font-bold text-2xl'>Chào mừng tới iVote</h3>
+              <p className='text-gray-600 pt-2'>Đăng nhập</p>
+            </section>
+            <section className='mt-10'>
+              <form className='flex flex-col' action="">
+                <div className='mb-6 pt-3 rounded bg-gray-200'>
+                  <label htmlFor="username" className='block text-gray-700 text-sm font-bold mb-2 ml-3'>Tên tài khoản</label>
+                  <input type="text" id='username' className='bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3' />
+                </div>
+                <div className='mb-6 pt-3 rounded bg-gray-200'>
+                  <label htmlFor="password" className='block text-gray-700 text-sm font-bold mb-2 ml-3'>Mật khẩu</label>
+                  <input type="password" id='password' className='bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3' />
+                </div>
+                <div className='pb-6 text-center'>
+                  <a href="" className='text-sm text-blue-600 hover:blue-700 hover:underline mb-6'>Quên mật khẩu?</a>
+                </div>
+                <button type='submit' className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200'>Đăng nhập</button>
+              </form>
+            </section>
+        </div>
+        <Footer/>
+        <div className='py-[200px]'>
+        </div>
+      </div>
+    </>
+    
+  )
+}
+
+export default login
