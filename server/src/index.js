f9c180986cf957ef3ae5dba6301455c745757a4e
Independence <nguyenkhanh17112003@gmail.com>
2023-03-27 12:41:12 +0700

    create user

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+const start = async function () {
+    try {
+        await app.listen(PORT, function () {
+            return console.log(`http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        throw new Error("Error: " + error);
+    }
+};
+
+(function () {
+    start();
+})();
